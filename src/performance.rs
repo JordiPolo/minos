73e10784b6889e0cc9fc3b519a1081934ed70ea9
Jordi Polo <mumismo@gmail.com>
2020-07-02 15:10:30 -0700

    Use latest Goose with rustls option

--- src/performance.rs
+++ src/performance.rs
@@ -12,13 +12,17 @@ lazy_static! {
 
 pub fn run(scenarios: &Vec<scenario::Scenario<'_>>, service: &service::Service, users: usize) {
 
+    // TODO: I need to put all the defaults here because the method that set them up also sets stuff I do not want.
     let mut configuration: GooseConfiguration = Default::default();
     configuration.hatch_rate = 1;  // Make one user per second
     configuration.users = Some(users); // How many users accessing simultaneously
     configuration.run_time = (users + 60).to_string(); // End test after 1 minute after users all online
     configuration.reset_stats = true; // Stats reset after hatching is completed so only count final 1 minute
     configuration.status_codes = false; // Add or not stats about status codes
+    configuration.stats_log_format = "json".to_string();
     configuration.log_file = "/tmp/minos_performance".to_string();
+    configuration.debug_log_format = "json".to_string();
+    configuration.debug_log_file = "/tmp/minos_performance".to_string();
     configuration.host = "http://localhost:3000".to_string(); // Dummy. Goose needs this. we overwrite urls later
     let goose_attack = GooseAttack::initialize_with_config(configuration).setup();
 
